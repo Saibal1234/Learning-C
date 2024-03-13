a2b3745f5d5c7a4d23ec2ff2fd2a028881a0936f
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-03-13 13:21:57 +0530

    My sixth problem

---
+++ Codchef/Course/LBCL05_problems_CLB024.c
@@ -0,0 +1,15 @@
+// Write a program which does the following
+
+// Declare two integer variables a and b
+// Assign the value 23 to a and 20 to b
+// Output the sum of a and b to the console
+
+#include <stdio.h>
+
+int main() {
+int a=23;
+    int b=20;
+    
+    printf("%d\n",a+b);
+}
+
