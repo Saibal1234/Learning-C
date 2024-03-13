451c9c2152babcb1b3f4ca2fcce0a825b53cb6d8
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-03-13 13:38:11 +0530

    My sixth problem

---
+++ Codchef/Course/LBCL05_problems_CLB025.c
@@ -0,0 +1,13 @@
+// Write a program which does the following
+
+// Find out and display the area of a rectangle of sides 45 and 76 respectively.
+
+#include <stdio.h>
+int main() {
+
+  int length = 45;
+  int width = 76;
+  int area = length * width;
+  printf("The Area of the given rectangle is %d",area);
+
+}
