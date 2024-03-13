2f4c07812f95bddc046183a1bbc008a084fcb67a
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-03-13 13:06:18 +0530

    My fifth problem

---
+++ Codchef/Course/LBCL04_problems_CLB019.c
@@ -0,0 +1,8 @@
+//Write the exact code which we discussed in this lesson, to output the sum of 12 and 7
+
+// Write complete code to print the sum of 12 and 7
+#include<stdio.h>
+int main(){
+    printf("%d",12+7);
+    return 0;
+}
