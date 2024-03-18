6477987cf04f35084614a7c755a796c9976f7df6
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-03-18 22:31:05 +0530

    First practice prob

---
+++ Codchef/practice/PCL01_problems_START01.c
@@ -0,0 +1,11 @@
+// Write a program that takes a number N as the input, and prints it to the output.
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+int num;
+scanf("%d",&num);
+printf("%d",num);
+}
+
