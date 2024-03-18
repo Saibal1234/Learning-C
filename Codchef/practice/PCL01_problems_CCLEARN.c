e337013f82e334e31ce63ef59ca90b2aeda9efe7
Saibal1234 <saibalroy587@gmail.comgit config --global user.name>
2024-03-18 23:02:43 +0530

    Fourth practice prob

---
+++ Codchef/practice/PCL01_problems_CCLEARN.c
@@ -0,0 +1,12 @@
+// The only line of input will contain a single integer N, denoting the number of languages for which there are courses.
+// Output on a single line the total number of courses in the section.
+
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+ int N;
+ scanf("%d",&N);
+ printf("%d",N*2);
+}
+
